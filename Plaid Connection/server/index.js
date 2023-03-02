0a3d85440fe47b57c593b34db6ca1feaca7d76da
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-03-02 14:03:19 -0500

    Remove secret

--- Plaid Connection/server/index.js
+++ Plaid Connection/server/index.js
@@ -7,8 +7,8 @@ const configuration = new Configuration({
     basePath: PlaidEnvironments.sandbox,
     baseOptions: {
         headers: {
-            'PLAID-CLIENT-ID':'63f6c22a7f1ecd0013a1e9c7',
-            'PLAID-SECRET':'2d13c1de5e408b6f07f3ac6a6017db',
+            'PLAID-CLIENT-ID':'',
+            'PLAID-SECRET':'',
         },
     },
 });
