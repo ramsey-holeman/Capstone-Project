0a9ac6e76a33cd7042cf3215d46a71b0d7a048e4
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-03-08 19:49:22 -0500

    Added chart based on users portfolio

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ session_start();
     include "db_connect.php";
     include "functions.php";
     $user_data = check_login($conn);
-
+    
+    $query = "SELECT ticker, share_num FROM stocks";
+    $result = mysqli_query($conn, $query);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,10 +31,35 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Dashboard</title>
     <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+    <script type="text/javascript" style="margin-left: auto; margin-right: auto">  
+    google.charts.load('current', {'packages':['corechart']});  
+    google.charts.setOnLoadCallback(drawChart);  
+    function drawChart()  
+    {  
+        var data = google.visualization.arrayToDataTable([  
+                    ['ticker', 'share_num'],  
+                    <?php  
+                    while($row = mysqli_fetch_array($result))  
+                    {  
+                        echo "['".$row["ticker"]."', ".$row["share_num"]."],";  
+                    }  
+                    ?>  
+                ]);  
+        var options = {  
+                title: 'Stocks in your portfolio',  
+                //is3D:true,  
+                pieHole: 0.4  
+                };  
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+        chart.draw(data, options);  
+    }  
+    </script>  
 </head>
 <body>
     <h1>Portfolio Dashboard</h1>
     <h4>Hello, <?php echo $user_data['first_name']; echo " "; echo $user_data['last_name']; ?>! Welcome back!</h4>
     
+    <div id="piechart" style="width: 900px; height: 500px;"></div>  
 </body>
 </html>
