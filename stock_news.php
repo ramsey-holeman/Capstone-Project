873e7473079332556d0be83b716515b0d584c2d3
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-04-06 02:28:40 -0400

    Works now

--- stock_news.php
+++ stock_news.php
@@ -47,46 +47,48 @@ session_start();
 
       <input type="submit" value="Search">
     </form>
-  <?php
-  if(isset($_POST['ticker'])){
-  $apiKey = '6efc26598c705a46c16082b0640c7c0f';
-  $searchTerm = '' . $_POST['ticker'];
-  $limit = $_POST['num_news'];
-  }
-  else{
-    $searchTerm = null;
-    $limit = 0;
-  }
-  // Initialize variables
-  $count = 0;
-  
-  // Loop through the pages of results until the limit is reached or all results are fetched
-  while ($count <= $_POST['num_news']){
-    // Build the API URL for the current page of results
-    $url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=$searchTerm&page=0&apikey=$apiKey";
-    
-    // Fetch the results from the API
-    $response = file_get_contents($url);
-    
-    // Parse the JSON response into an array
-    $data = json_decode($response, true);
-    
-    // Loop through the news articles and print the title and summary
-    foreach ($data as $article) {
-      // Print the title and summary
-      echo "<h2>{$article['title']}</h2>";
-      echo "<p>{$article['text']}</p>";
-      echo "<a href='{$article['url']}'>Click here for the article</a>";
-      
-      // Increment the count of articles displayed
-      $count++;
-      
-      // If we have reached the limit, break out of the loop
-      if ($count >= $limit) {
-        break;
-      }
+<?php
+  if(array_key_exists("ticker", $_POST)) {
+    // Set the API key
+    $api_key = "6efc26598c705a46c16082b0640c7c0f";
+    // Set the stock ticker to search for
+    $ticker = $_POST['ticker'];
+    // Set the maximum number of articles to display
+    $max_articles = $_POST['num_news'];
+
+    // Set the counter variable to zero
+    $count = 0;
+
+    // Loop until the counter is less than the maximum number of articles
+    while ($count < $max_articles) {
+
+        // Make the API request
+        $request_url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=$ticker&apikey=$api_key";
+        $response = file_get_contents($request_url);
+
+        // Decode the JSON response
+        $json = json_decode($response, true);
+
+        // Iterate over the news articles and display them
+        foreach ($json as $article) {
+            echo "<h3>" . $article["title"] . "</h3>";
+            echo "<p>" . $article["text"] . "</p>";
+            echo "<p><a href=\"" . $article["url"] . "\">Read more</a></p>";
+            echo "<hr>";
+            $count++;
+
+            // Check if the counter is equal to the maximum number of articles
+            if ($count == $max_articles) {
+                break 2;
+            }
+        }
+
+        // Wait for 1 second before making the next API request
+        sleep(1);
     }
-   }
-  ?>
+}else{
+  $ticker = null;  // no color given; default to red
+}
+?>
   
 </body>
