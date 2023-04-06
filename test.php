75516efff31406fe580a206ac7b614336cce77df
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-04-06 02:28:50 -0400

    Testing.....

--- test.php
+++ test.php
@@ -1,79 +1,70 @@
-<form method="post">
-    <label for="keywords">Enter keywords to search for:</label>
-    <input type="text" name="keywords" id="keywords">
-    <input type="submit" value="Search">
-  </form>
-
-  <?php
-  // Check if the form has been submitted
-  if (isset($_POST['keywords'])) {
-    // Get the user input from the form
-    $keywords = $_POST['keywords'];
-  
-    // Set the API endpoint URL with the user input
-    $url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" . urlencode($keywords) . "&apikey=99AZK8RLIWQZZQA6";
-  
-    // Send a GET request to the API endpoint and retrieve the JSON response
-    $response = file_get_contents($url);
-  
-    // Decode the JSON response into a PHP associative array
-    $search_results = json_decode($response, true)["bestMatches"];
-  
-    // Output the search results
-      echo "<table>";
-      foreach ($search_results as $result) {
-      echo "<tr>";
-      echo "<td>" . $result['1. symbol'] . "</td>";
-      echo "<td>" . $result['2. name'] . "</td>";
-      echo "<td><button onclick=\"selectTicker('" . $result['1. symbol'] . "')\">Select</button></td>" . "<br>";
-      echo "</tr>";
-    }
-    echo "</table>";
-  }
-
-
-//   // Check if the form has been submitted
-// if (isset($_POST['ticker'])) {
-//   // Get the user input from the form and sanitize it
-//   $ticker = $_POST['ticker'];
-
-//   // Validate the ticker symbol
-//   if (preg_match('/^[A-Z]{1,5}$/', $ticker)) {
-//         // Prepare the SQL statement
-//     $stmt = $conn->prepare("INSERT INTO portfolio (ticker) VALUES (?)");
-
-//     // Bind the ticker symbol parameter and execute the statement
-//     $stmt->bind_param("s", $ticker);
-//     $stmt->execute();
-
-//     // Close the statement and the connection
-//     $stmt->close();
-//     $conn->close();
-
-//     // Output a success message
-//     echo "Ticker symbol added to portfolio!";
-//   } else {
-//     // Output an error message if the ticker symbol is invalid
-//     echo "Invalid ticker symbol. Please try again.";
-//   }
-// }
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<title>Dashboard</title>
+<head>    
+  <header>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="skeleton.css">
+    <div id="wrap">
+        <nav>
+            <ul class="navbar">
+                <a href="index.php">Dashboard</a>
+                <a href="portfolio_edit.php">Edit Portfolio</a>
+                <a href="options.php">Stock Options</a>
+                <a href="stock_news.php">News</a>
+                <a href="login_page.php">Login</a><br>
+                <a href="logout.php">Logout</a>
+            </ul>
+        </nav>
+    </div>    
+  </header>
+</head>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stock News</title>
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>   
+</head>
+<body>
 <?php
-// Set the API endpoint and parameters
-$endpoint = "https://www.alphavantage.co/query";
-$function = "GLOBAL_QUOTE"; // The API function to retrieve the latest stock price
-$symbol = "AAPL"; // The stock symbol to retrieve the price for
-$apikey = "99AZK8RLIWQZZQA6"; // Your Alpha Vantage API key
+// Set the API key
+$api_key = "6efc26598c705a46c16082b0640c7c0f";
+// Set the stock ticker to search for
+$ticker = "AAPL";
+// Set the maximum number of articles to display
+$max_articles = 5;
 
-// Make the API request
-$url = "$endpoint?function=$function&symbol=$symbol&apikey=$apikey";
-$response = file_get_contents($url);
+// Set the counter variable to zero
+$count = 0;
 
-// Parse the JSON response and extract the latest stock price
-$result = json_decode($response, true);
-$latestPrice = $result["Global Quote"]["05. price"];
+// Loop until the counter is less than the maximum number of articles
+while ($count < $max_articles) {
 
-// Do something with the latest stock price, such as saving it to a database or displaying it on your website
-echo "The latest price for $symbol is $latestPrice";
+    // Make the API request
+    $request_url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=$ticker&apikey=$api_key";
+    $response = file_get_contents($request_url);
+
+    // Decode the JSON response
+    $json = json_decode($response, true);
+
+    // Iterate over the news articles and display them
+    foreach ($json as $article) {
+        echo "<h3>" . $article["title"] . "</h3>";
+        echo "<p>" . $article["text"] . "</p>";
+        echo "<p><a href=\"" . $article["url"] . "\">Read more</a></p>";
+        echo "<hr>";
+        $count++;
+
+        // Check if the counter is equal to the maximum number of articles
+        if ($count == $max_articles) {
+            break 2;
+        }
+    }
+
+    // Wait for 1 second before making the next API request
+    sleep(1);
+}
 ?>
+</body>
